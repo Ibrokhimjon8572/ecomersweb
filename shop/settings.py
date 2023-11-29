6ad3403bf1cd857d28fa7c6dd298672a28b10c59
Mamajonov Ibrohimjon <mamajonovibrokhimjon@gmail.com>
2023-11-29 20:37:04 +0500

    add/category card

--- shop/settings.py
+++ shop/settings.py
@@ -117,7 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'product/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / ''
 # MEDIA_URL = '/images/'
 
 # STATICFILES_DIRS = [
@@ -125,7 +128,7 @@ STATIC_URL = 'static/'
 # ]
 
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
